b7ac4f8a3f5d7d52ac8c6544ebb513c4fed7d187
Alexandru Goga <alexandrugoga12@gmail.com>
2024-11-11 01:52:15 +0200

    DeleteBackendV1

--- Backend/models/user.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class User(db.Model):
-    __tablename__ = 'User'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
