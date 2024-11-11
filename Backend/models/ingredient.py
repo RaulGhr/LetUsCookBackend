abcbf08ccbff7f348c0ec92aa9dc884ddf30dd3e
Alexandru Goga <126619451+AlexG1203@users.noreply.github.com>
2024-11-11 19:27:27 +0200

    Delete Backend directory

--- Backend/models/ingredient.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Ingredient(db.Model):
-    __tablename__ = 'Ingredient'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    measure_unit = db.Column(db.String(50))
